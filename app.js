66e23e43f573613fecbcace524661ccf412e2ed2
Israel Arunah <arunahisraelg2010@gmail.com>
2018-08-15 16:06:19 +0100

    Creating project structure

--- app.js
+++ app.js
@@ -1,13 +1,30 @@
 const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
+const helmet = require('helmet');
+const cors = require('cors');
+const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
+const flash = require('express-flash');
+const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const sassMiddleware = require('node-sass-middleware');
+require('dotenv').config();
 
-const usersRouter = require('./routes/users');
+const usersRouter = require('./routes/usersRouter');
+const indexRouter = require('./routes/indexRouter');
 
 const app = express();
+app.use(helmet());
+app.use(cors());
+
+mongoose.Promise = require('bluebird');
+const db_connection = mongoose.connection;
+mongoose.connect(process.env.DB_URL,  {useNewUrlParser: true})
+    .catch(err => console.error(err));
+db_connection.on('error', console.error.bind(console, 'MongoDB connection error: '));
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -17,6 +34,26 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+app.set('trust proxy', 1); // trust first proxy
+
+//use sessions for tracking logins
+let session_data =
+    {
+        secret: process.env.SESSION_SECRET,
+        resave: false,
+        saveUninitialized: true,
+        cookie: {secure: true},
+        expires: 3600000,
+        store: new MongoStore({
+            mongooseConnection: db_connection
+        })
+    };
+
+app.use(session(session_data));
+app.use(flash());
+
+
 app.use(sassMiddleware({
   src: path.join(__dirname, 'public'),
   dest: path.join(__dirname, 'public'),
---
+++ constants/responseMessages.js
@@ -0,0 +1,5 @@
+module.exports = {
+    ERROR_OCCURRED: 'An error occurred, please try again',
+
+    METHOD_NOT_IMPLEMENTED: 'Method not implemented'
+};
